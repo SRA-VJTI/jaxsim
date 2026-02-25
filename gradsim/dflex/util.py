1dc650bc72f5ec671773e5934e3317ab9b3b2476
Sarayu Anantharaman <sarayusapa@gmail.com>
2026-02-25 14:52:26 +0530

    conv and tested 3 files

--- gradsim/dflex/util.py
+++ gradsim/dflex/util.py
@@ -29,7 +29,6 @@ import timeit
 import math
 import numpy as np
 import gc
-import torch
 import cProfile
 
 log_output = ""
@@ -416,51 +415,12 @@ class MeshAdjacency:
 
 
 def mem_report():
-    '''Report the memory usage of the tensor.storage in pytorch
-    Both on CPUs and GPUs are reported'''
-
-    def _mem_report(tensors, mem_type):
-        '''Print the selected tensors of type
-        There are two major storage types in our major concern:
-            - GPU: tensors transferred to CUDA devices
-            - CPU: tensors remaining on the system memory (usually unimportant)
-        Args:
-            - tensors: the tensors of specified type
-            - mem_type: 'CPU' or 'GPU' in current implementation '''
-        total_numel = 0
-        total_mem = 0
-        visited_data = []
-        for tensor in tensors:
-            if tensor.is_sparse:
-                continue
-            # a data_ptr indicates a memory block allocated
-            data_ptr = tensor.storage().data_ptr()
-            if data_ptr in visited_data:
-                continue
-            visited_data.append(data_ptr)
-
-            numel = tensor.storage().size()
-            total_numel += numel
-            element_size = tensor.storage().element_size()
-            mem = numel*element_size /1024/1024 # 32bit=4Byte, MByte
-            total_mem += mem
-            element_type = type(tensor).__name__
-            size = tuple(tensor.size())
-
-            # print('%s\t\t%s\t\t%.2f' % (
-            #     element_type,
-            #     size,
-            #     mem) )
-        print('Type: %s Total Tensors: %d \tUsed Memory Space: %.2f MBytes' % (mem_type, total_numel, total_mem) )
-
-    gc.collect()
-
-    LEN = 65
-    objects = gc.get_objects()
-    #print('%s\t%s\t\t\t%s' %('Element type', 'Size', 'Used MEM(MBytes)') )
-    tensors = [obj for obj in objects if torch.is_tensor(obj)]
-    cuda_tensors = [t for t in tensors if t.is_cuda]
-    host_tensors = [t for t in tensors if not t.is_cuda]
-    #_mem_report(cuda_tensors, 'GPU')
-    _mem_report(host_tensors, 'CPU')
-    print('='*LEN)
+    '''Report the memory usage of live JAX arrays.'''
+    try:
+        import jax
+        arrays = jax.live_arrays()
+        total_bytes = sum(a.nbytes for a in arrays)
+        print('Live JAX arrays: %d \tUsed Memory Space: %.2f MBytes' % (len(arrays), total_bytes / 1024 / 1024))
+    except Exception:
+        print('mem_report: unable to query JAX memory')
+    print('=' * 65)
